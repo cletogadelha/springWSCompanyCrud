e8f0beb2d1cb3e6245bdc211432560d22f3a6e1b
Cleto Gadelha <cletogadelha@gmail.com>
2016-02-08 18:55:29 -0200

    teste

--- src/main/java/com/cletogadelha/config/WebAppInitializer.java
+++ src/main/java/com/cletogadelha/config/WebAppInitializer.java
@@ -1,12 +1,15 @@
 package com.cletogadelha.config;
 
-import javax.servlet.ServletContext;  
-import javax.servlet.ServletException;  
-import javax.servlet.ServletRegistration.Dynamic;  
-  
-import org.springframework.web.WebApplicationInitializer;  
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
-import org.springframework.web.servlet.DispatcherServlet;  
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.cletogadelha.filters.CorsFilter;  
   
 public class WebAppInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {  
@@ -14,7 +17,10 @@ public class WebAppInitializer implements WebApplicationInitializer {
         ctx.register(AppConfig.class);  
         ctx.setServletContext(servletContext);    
         Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        dynamic.addMapping("/");  
+        dynamic.addMapping("/");
         dynamic.setLoadOnStartup(1);  
+        
+        FilterRegistration.Dynamic corsFilter = servletContext.addFilter("corsFilter", CorsFilter.class);
+        corsFilter.addMappingForUrlPatterns(null, false, "/*");
    }  
 }
